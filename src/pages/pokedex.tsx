7b5bbdffe751d6430967c05985fbbe2c960a6b0f
AldrinEstrada <aldrinestradabcit@gmail.com>
2024-02-19 21:38:30 -0800

    pokedex update

--- src/pages/pokedex.tsx
+++ src/pages/pokedex.tsx
@@ -10,11 +10,13 @@ interface Pokemon {
 export default function Pokedex(): JSX.Element {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
   const [offset, setOffset] = useState(0);
+  const limit = 100;
+  
 
   useEffect(() => {
     const fetchPokemons = async () => {
       const allPokemonData: any[] = [];
-      let response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=100&offset=${offset}`);
+      let response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
       let data = await response.json();
       for (const pokemon of data.results) {
         response = await fetch(pokemon.url);
@@ -22,7 +24,7 @@ export default function Pokedex(): JSX.Element {
         allPokemonData.push({
           name: pokemonData.name,
           id: pokemonData.id,
-          imageUrl: pokemonData.sprites.front_default
+          imageUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${pokemonData.id}.gif`
         });
       }
       setPokemons(allPokemonData);
@@ -32,23 +34,26 @@ export default function Pokedex(): JSX.Element {
   }, [offset]);
 
   const loadNextPage = () => {
-    setOffset(prevOffset => prevOffset + 100);
+    setOffset(prevOffset => prevOffset + limit); // Increment by the limit
+    window.scrollTo({ top: 0, behavior: 'auto' });
   };
 
   return (
     <div>
-      <SimpleGrid columns={5} spacing={10}>
+      <SimpleGrid columns={5} spacing={12} padding={40}>
         {pokemons.map(pokemon => (
-          <Card key={pokemon.id}>
+          <Card key={pokemon.id} w={270} h={180} position="relative" boxShadow="0 4px 8px 0 rgba(0,0,0,0.2), 4px 0 8px 0 rgba(0,0,0,0.2)">
             <CardHeader>
-              <Heading size='md'>{pokemon.name}</Heading>
-              <Text>#{pokemon.id}</Text>
+            <Heading size='md'>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</Heading>
+            <Text fontWeight="bold" color="gray" fontSize={20}>#{pokemon.id}</Text>
             </CardHeader>
-            <Image src={pokemon.imageUrl} alt={pokemon.name} />
+            <Image src={pokemon.imageUrl} alt={pokemon.name} boxSize="150px" objectFit="none" transform="scale(1.7)" position="absolute" bottom="1" right="0"/>
           </Card>
         ))}
       </SimpleGrid>
-      <Button onClick={loadNextPage}>Load Next 100 Pokémon</Button>
+      {pokemons.length < 649 && ( 
+        <Button onClick={loadNextPage}>Load Next 100 Pokémon</Button>
+      )}
     </div>
   );
 }
