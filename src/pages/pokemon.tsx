e01b11ad2b8369f806683008d9da027c301b0361
wendyychenn <wchen242@my.bcit.ca>
2024-03-23 19:24:30 -0700

    adding buttons and spacing adjustment

--- src/pages/pokemon.tsx
+++ src/pages/pokemon.tsx
@@ -1,6 +1,5 @@
 import Nav from '@/components/Nav';
-import { Box, Flex, Text, SimpleGrid, Heading, Image, Divider } from '@chakra-ui/react';
-import Link from 'next/link';
+import { Box, Flex, Text, SimpleGrid, Heading, Image, Divider, Button, Link } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
 import BackButton from '@/components/BackButton';
@@ -53,19 +52,19 @@ export default function Pokemon(): JSX.Element {
             <Image src={pokemonDetails.sprites.other['official-artwork'].front_default} alt="Official Artwork" boxSize="200px" mt={4} />
           )}
         </Flex>
-        <Flex flex={6} alignItems="center" direction="column">
+        <Flex flex={6} alignItems="flex-start" direction="column" marginLeft='7rem'>
           {pokemonDetails && (
             <>
-              <Heading as="h1" fontSize="2xl" mt={10}>{pokemonDetails.name}</Heading>
-              <Text>Type?</Text>
+              <Heading as="h1" fontSize="2xl" mt={10} style={{ textTransform: 'capitalize' }}>{pokemonDetails.name}</Heading>
+              <Button my='2' bg='#8BC5CD' color='white' size='xs' fontWeight='normal'>Type?</Button>
               <SimpleGrid alignItems={'center'} textAlign='center' columns={2} spacing={4} mt={4}>
                 <Box rounded='lg' boxShadow='lg' py='6'>
                   <Text color={'#A0A0A0'}>Species</Text>
-                  <Text color={'#3AC291'}>{pokemonDetails.species.name}</Text>
+                  <Text color={'#3AC291'} style={{ textTransform: 'capitalize' }}>{pokemonDetails.species.name}</Text>
                 </Box>
                 <Box rounded='lg' boxShadow='lg' py='6'>
                   <Text color={'#A0A0A0'}>Abilities</Text>
-                  <Text color={'#3AC291'}>{pokemonDetails.abilities.map(ability => ability.ability.name).join(', ')}</Text>
+                  <Text color={'#3AC291'} style={{ textTransform: 'capitalize' }}>{pokemonDetails.abilities.map(ability => ability.ability.name).join(', ')}</Text>
                 </Box>
                 <Box rounded='lg' boxShadow='lg' py='6'>
                   <Text color={'#A0A0A0'}>Height</Text>
@@ -77,10 +76,9 @@ export default function Pokemon(): JSX.Element {
                 </Box>
               </SimpleGrid>
              
-              <Divider my={4} w='55%' h='20px'/>
-              <Text>bla</Text>
-              <Divider my={4} w='55%' />
+              <Text my='8' style={{ textTransform: 'capitalize' }}>bla</Text>
             
+              <Button bg='#8BC5CD' color='white' size='lg' fontWeight='normal'><Link href='/pokedex'>Explore More Pokemon</Link></Button>
             </>
           )}
         </Flex>
