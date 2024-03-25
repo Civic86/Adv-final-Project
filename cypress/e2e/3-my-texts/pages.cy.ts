286667ea16c85274b4caeafe36f5ed7260b19f79
anoncoderin <113314047+anoncoderin@users.noreply.github.com>
2024-03-25 14:50:00 -0700

    tests

--- cypress/e2e/3-my-texts/pages.cy.ts
+++ cypress/e2e/3-my-texts/pages.cy.ts
@@ -1,4 +1,9 @@
 describe('Navigation', () => {
+
+    beforeEach(() => {
+        cy.viewport(1280, 720) // Set viewport to 1280px x 720px
+      })
+
     it('should navigate to pokedex page and check text', () => {
       // Start from index page
       cy.visit('http://localhost:3000');
@@ -28,16 +33,16 @@ describe('Navigation', () => {
 
 
     it('should search for Pokemon by name', () => {
-        // Visit the page where the Pokedex is located
+        
         cy.visit('http://localhost:3000/pokedex')
     
-        // Ensure that the Pokedex header is visible and has the correct text and styling
+        // Check Pokedex header is visible and has the correct text and styling
         cy.get('p').should('have.css', 'font-size', '30px')
           .and('have.css', 'font-weight', '600')
           .and('have.css', 'color', 'rgb(90, 201, 161)')
           .and('have.text', 'Pokedex')
     
-        // Ensure that the input field is visible and functional
+        // input field is visible and functional
         cy.get('input[type="string"]').should('be.visible')
           .and('have.attr', 'placeholder', 'Search by Pokemon name, id')
     
@@ -45,17 +50,37 @@ describe('Navigation', () => {
         const searchTerm = 'Charmeleon' 
         cy.get('input[type="string"]').type(searchTerm)
     
-           cy.wait(500)
-    
+           cy.wait(500)    
       })
 
       it('should display Pokemon cards in a grid layout', () => {
-        // Visit the page where the Pokedex is located
-        cy.visit('http://localhost:3000/pokedex')
-    
-        // Ensure that the Pokemon cards are displayed in a grid layout
-        cy.get('div').should('exist')
+               cy.visit('http://localhost:3000/pokedex')
+            cy.get('div').should('exist')
       })
+
+
+
+
+      it('should have button, image, h1, p, div', () => {
+        cy.visit('http://localhost:3000/pokemon?name=togepi');
+       
+        cy.get('Button').should('exist')
+    cy.get('img').should('exist')
+    cy.get('h1').should('exist')
+    cy.get('p').should('exist')
+    cy.get('div').should('exist')
+    cy.get('div').should('exist')
+    cy.get('Button').should('exist')
+    })
+
+    it('type', () => {
+        cy.visit('http://localhost:3000/pokedex');
+
+    const searchQuery = 'Gardevoir-Mega'; 
+    cy.get('input[type="string"]').type(searchQuery)
+    cy.get('input[type="string"]').should('have.value', searchQuery)
+
+    })
 
   });
   
