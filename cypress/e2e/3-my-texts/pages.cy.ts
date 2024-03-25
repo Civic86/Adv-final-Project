d9d2a5573dc36498bd38eef6aa86374a6af7de9b
anoncoderin <113314047+anoncoderin@users.noreply.github.com>
2024-03-25 14:17:17 -0700

    more tesing added

--- cypress/e2e/3-my-texts/pages.cy.ts
+++ cypress/e2e/3-my-texts/pages.cy.ts
@@ -25,7 +25,37 @@ describe('Navigation', () => {
 
         cy.get('button').should('have.length', '1');
     })
+
+
+    it('should search for Pokemon by name', () => {
+        // Visit the page where the Pokedex is located
+        cy.visit('http://localhost:3000/pokedex')
     
+        // Ensure that the Pokedex header is visible and has the correct text and styling
+        cy.get('p').should('have.css', 'font-size', '30px')
+          .and('have.css', 'font-weight', '600')
+          .and('have.css', 'color', 'rgb(90, 201, 161)')
+          .and('have.text', 'Pokedex')
+    
+        // Ensure that the input field is visible and functional
+        cy.get('input[type="string"]').should('be.visible')
+          .and('have.attr', 'placeholder', 'Search by Pokemon name, id')
+    
+       
+        const searchTerm = 'Charmeleon' 
+        cy.get('input[type="string"]').type(searchTerm)
+    
+           cy.wait(500)
+    
+      })
+
+      it('should display Pokemon cards in a grid layout', () => {
+        // Visit the page where the Pokedex is located
+        cy.visit('http://localhost:3000/pokedex')
+    
+        // Ensure that the Pokemon cards are displayed in a grid layout
+        cy.get('div').should('exist')
+      })
 
   });
   
